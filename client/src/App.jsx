16f77e4d183673906f9b17715861fb100b0b71ad
Ishan Gupta <ishan.23fe10cai00277@muj.manipal.edu>
2024-09-28 17:25:53 +0530

    header changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-
+import {styled} from "styled-components";
 import './App.css'
 
 function App() {
 
+  const H1=styled.h1``;
+
   return (
     <>
-
+      <H1>HELLO ,This is Ishan</H1>
     </>
   )
 }
